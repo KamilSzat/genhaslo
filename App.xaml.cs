c5b5908f90ced48f5ced684ba0e175bf4769f3a7
KamilSzat <uT_3776@zsp3zamosc.pl>
2025-05-22 09:35:13 +0200

    Zalążek programu

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace genhaslo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace genhaslo
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
