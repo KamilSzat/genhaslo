5183a83a2ca7f7dd97b43babe12ff0e242c6ef4d
KamilSzat <uT_3776@zsp3zamosc.pl>
2025-05-26 13:48:25 +0200

    Kod z egzaminu

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,25 +1,58 @@
-﻿namespace genhaslo
+﻿using System.Text;
+
+namespace genhaslo
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
 
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private void OnGenerujClicked(object sender, EventArgs e)
         {
-            count++;
+            int.TryParse(dlugoscEntry.Text, out int dlugosc);
+            if (dlugosc <= 0) return;
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+            string male = "abcdefghijklmnopqrstuvwxyz";
+            string wielkie = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string cyfry = "0123456789";
+            string znaki = "!@#$%^&*()";
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            StringBuilder dostepne = new StringBuilder();
+            if (maleWielkieCheck.IsChecked == true)
+                dostepne.Append(male + wielkie);
+            if (cyfryCheck.IsChecked == true)
+                dostepne.Append(cyfry);
+            if (znakiSpecjalneCheck.IsChecked == true)
+                dostepne.Append(znaki);
+
+            if (dostepne.Length == 0)
+            {
+                wygenerowaneHaslo.Text = "Wybierz przynajmniej jedną opcję.";
+                return;
+            }
+
+            var rnd = new Random();
+            var haslo = new StringBuilder();
+            for (int i = 0; i < dlugosc; i++)
+            {
+                int index = rnd.Next(dostepne.Length);
+                haslo.Append(dostepne[index]);
+            }
+
+            wygenerowaneHaslo.Text = haslo.ToString();
+        }
+
+        private void OnZatwierdzClicked(object sender, EventArgs e)
+        {
+            string komunikat = $"Dane pracownika:\n" +
+                               $"Imię: {imieEntry.Text}\n" +
+                               $"Nazwisko: {nazwiskoEntry.Text}\n" +
+                               $"Stanowisko: {stanowiskoPicker.SelectedItem}\n" +
+                               $"Hasło: {wygenerowaneHaslo.Text}";
+            DisplayAlert("Zatwierdzono", komunikat, "OK");
         }
     }
-
 }
